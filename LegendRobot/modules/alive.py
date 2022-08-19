987a0e6212dc982a460dd1151f530ac3316e399c
Lawless_Shikari <91068760+ShikariBaaZ@users.noreply.github.com>
2022-08-19 20:57:18 +0530

    Create alive.py

---
+++ LegendRobot/modules/alive.py
@@ -0,0 +1,36 @@
+import os
+import re
+import random
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from LegendRobot.events import register
+from LegendRobot import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatcher
+
+
+PHOTO = [
+    "https://telegra.ph/file/d7fe94d4999330896c0ef.jpg",
+    "https://telegra.ph/file/d7fe94d4999330896c0ef.jpg",
+]
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [SHIKARii](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("ʜᴇʟᴘ​", f"https://t.me/{dispatcher.bot.username}?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/{SUPPORT_CHAT}"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+__mod_name__ = "Aʟɪᴠᴇ"
