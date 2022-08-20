07bd0912769c572a2f5d9097e22dfb7fa139fef8
Lawless_Shikari <91068760+ShikariBaaZ@users.noreply.github.com>
2022-08-20 12:51:16 +0530

    Create writetool.py

---
+++ LegendRobot/modules/writetool.py
@@ -0,0 +1,66 @@
+import requests
+from requests import get
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from LegendRobot import pbot as Legend, BOT_NAME, BOT_USERNAME
+
+
+@Legend.on_message(filters.command("write"))
+async def handwrite(_, message: Message):
+    if not message.reply_to_message:
+        text = message.text.split(None, 1)[1]
+        m = await Legend.send_message(
+            message.chat.id, "`Please wait...,\n\nWriting your text...`"
+        )
+        API = f"https://api.sdbots.tk/write?text={text}"
+        req = requests.get(API).url
+        caption = f"""
+Successfully Written Text 💘
+
+✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
+🥀 **Requested by :** {message.from_user.mention}
+❄ **Link :** `{req}`
+"""
+        await m.delete()
+        await Legend.send_photo(
+            message.chat.id,
+            photo=req,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{req}")]]
+            ),
+        )
+    else:
+        lol = message.reply_to_message.text
+        m = await Legend.send_message(
+            message.chat.id, "`Please wait...,\n\nWriting your text...`"
+        )
+        API = f"https://api.sdbots.tk/write?text={lol}"
+        req = requests.get(API).url
+        caption = f"""
+Successfully Written Text 💘
+
+✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
+🥀 **Requested by :** {message.from_user.mention}
+❄ **Link :** `{req}`
+"""
+        await m.delete()
+        await Legend.send_photo(
+            message.chat.id,
+            photo=req,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{req}")]]
+            ),
+        )
+
+
+__mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
+
+__help__ = """
+
+ Writes the given text on white page with a pen 🖊
+
+❍ /write <text> *:* Writes the given text.
+ """
