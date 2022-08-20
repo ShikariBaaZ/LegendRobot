078f81f8d94edd2e857d4c5fca7f7265cd6a4dd2
Lawless_Shikari <91068760+ShikariBaaZ@users.noreply.github.com>
2022-08-20 12:28:02 +0530

    Create source.py

---
+++ LegendRobot/modules/source.py
@@ -0,0 +1,43 @@
+from platform import python_version as y
+from telegram import __version__ as o
+from pyrogram import __version__ as z
+from telethon import __version__ as s
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters
+from LegendRobot import pbot as client, dispatcher, OWNER_USERNAME
+
+
+Shikari = "https://telegra.ph/file/66f4cbd1978ddd57cd034.jpg"
+
+
+@client.on_message(filters.command(["repo", "source"]))
+async def repo(client, message):
+    await message.reply_photo(
+        photo=ANON,
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [Shikari](tg://user?id=2015883285)
+**» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
+**» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
+**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
+**» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
+
+**ꜰᴀʟʟᴇɴ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ᴏᴡɴᴇʀ •", url=f"https://t.me/{OWNER_USERNAME}"
+                    ),
+                    InlineKeyboardButton(
+                        "• sᴏᴜʀᴄᴇ •",
+                        url="https://github.com/ShikariBaaZ/LegendRobot",
+                    ),
+                ]
+            ]
+        ),
+    )
+
+
+__mod_name__ = "Rᴇᴩᴏ"
