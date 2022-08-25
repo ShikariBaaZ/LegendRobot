3d5202226256a5cc8e256de61e4982318dbd600d
ShikariBaaZ <ShikariBaaZ@users.noreply.github.com>
2022-08-25 13:14:39 +0000

    Automated code formatting
    Signed-off-by: ShikariBaaZ <102808014+ShikariBaaZ@users.noreply.github.com>

--- LegendRobot/sample_config.py
+++ LegendRobot/sample_config.py
@@ -19,7 +19,9 @@ class Config(object):
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "The_Shikari"
-    SUPPORT_CHAT = "ShikariSupportNetwork"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = (
+        "ShikariSupportNetwork"  # Your own group for support, do not add the @
+    )
     JOIN_LOGGER = (
         -1001686672798
     )  # Prints any new group the bot is added to, prints just the name and ID.
